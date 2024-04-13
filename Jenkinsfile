fcd8b3a9b17a69dde73d3321374043e3810627d9
josepharodi20 <josepharodi20@gmail.com>
2024-04-13 21:51:03 +0300

    Added Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,23 @@
+pipeline {
+    tools{
+        nodejs 'nodejs'
+    }
+    agent any
+    stages{
+        stage('clone-repo'){
+            steps{
+                git branch:'master', url: 'https://github.com/whitearodi/gallery2.git'
+            }
+        }
+        stage('build-project'){
+            steps{
+               sh 'npm install'
+            }
+        }
+        stage('test-project'){
+            steps{
+                sh 'npm test'
+            }
+        }
+    }
+}
--- _config.js
+++ _config.js
@@ -1,9 +1,15 @@
 var config = {}
 
 // Update to have your correct username and password
+// config.mongoURI = {
+//     production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+//     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+//     test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+// }
+
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://delonowano:Hallo200!@cluster-gallery.f6f4vuh.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Cluster-gallery',
+    development: 'mongodb+srv://delonowano:Hallo200!@cluster-gallery.f6f4vuh.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=Cluster-gallery',
+    test: 'mongodb+srv://delonowano:Hallo200!@cluster-gallery.f6f4vuh.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=Cluster-gallery',
 }
 module.exports = config;
